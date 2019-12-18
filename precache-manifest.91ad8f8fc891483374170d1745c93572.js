e0256955db1ad92571e7d002dcaa89db2681ecd2
Sorin Chis <kish.sorin@yahoo.com>
2019-12-18 11:50:54 +0200

    Updates

---
+++ precache-manifest.91ad8f8fc891483374170d1745c93572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb06d6049dbe70782facb0ed4236751",
+    "url": "/SorinC6/Image-Gallery/index.html"
+  },
+  {
+    "revision": "5916245c47993b5ba3e5",
+    "url": "/SorinC6/Image-Gallery/static/css/main.9cb40cbe.chunk.css"
+  },
+  {
+    "revision": "1aef9796ef399de5fe2e",
+    "url": "/SorinC6/Image-Gallery/static/js/2.0a38fed9.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/SorinC6/Image-Gallery/static/js/2.0a38fed9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5916245c47993b5ba3e5",
+    "url": "/SorinC6/Image-Gallery/static/js/main.4d2f234d.chunk.js"
+  },
+  {
+    "revision": "2cdb76e3355a718b1304",
+    "url": "/SorinC6/Image-Gallery/static/js/runtime-main.ccd82a17.js"
+  }
+]);
